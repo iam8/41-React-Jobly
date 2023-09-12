dbe75fd18b8e8484231f4ce48d1dbc73bdd01c2f
alexm <alex-m@outlook.com>
2023-09-12 15:31:21 -0700

    Import and render components for each route

--- src/routes/Routes.js
+++ src/routes/Routes.js
@@ -1,6 +1,14 @@
 import React from "react";
 import { Route, Switch, Redirect } from "react-router-dom";
 
+import Home from "../homepage/Home";
+import LoginForm from "../auth/LoginForm";
+import SignupForm from "../auth/SignupForm";
+import Companies from "../companies/Companies";
+import Company from "../companies/Company";
+import Jobs from "../jobs/Jobs";
+import EditProfileForm from "../profiles/EditProfileForm";
+
 
 /**
  * All routes for Jobly website.
@@ -14,27 +22,31 @@ function Routes({login, signup}) {
     return (
         <Switch>
             <Route exact path="/companies/:handle">
-
+                <Company />
             </Route>
 
             <Route exact path="/companies">
-
+                <Companies />
             </Route>
 
             <Route exact path="/jobs">
-
+                <Jobs />
             </Route>
 
             <Route exact path="/login">
-
+                <LoginForm />
             </Route>
 
             <Route exact path="/signup">
-
+                <SignupForm />
             </Route>
 
             <Route exact path="/profile">
+                <EditProfileForm />
+            </Route>
 
+            <Route exact path="/">
+                <Home />
             </Route>
 
             <Redirect to="/" />
