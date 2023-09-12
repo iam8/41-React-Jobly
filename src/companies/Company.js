5637d95054b55299031a00b6bc218a40f39e76c6
alexm <alex-m@outlook.com>
2023-09-12 15:36:56 -0700

    Retrieve company name from URL using useParams

--- src/companies/Company.js
+++ src/companies/Company.js
@@ -1,11 +1,14 @@
 import React from "react";
+import { useParams } from "react-router-dom";
 
 
 function Company() {
+    const { handle } = useParams();
 
     return (
         <div>
             COMPANY DETAILS
+            Company name received: {handle}
         </div>
     )
 }
