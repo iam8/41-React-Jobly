3a97dc197fa98eba79a4f19ad70f85c07d960900
alexm <alex-m@outlook.com>
2023-09-12 16:47:49 -0700

    Add logic to fetch company data and display on page

--- src/companies/Company.js
+++ src/companies/Company.js
@@ -1,14 +1,42 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import { useParams } from "react-router-dom";
+
+import JoblyApi from "../api";
 
 
 function Company() {
     const { handle } = useParams();
+    const [companyData, setCompanyData] = useState(null);
+
+    // Fetch company data from API
+    useEffect(() => {
+
+        const getCompanyData = async () => {
+            try {
+                setCompanyData(await JoblyApi.getCompany(handle));
+            } catch(err) {
+                console.log(err);
+            }
+        }
+
+        getCompanyData();
+
+    }, [handle]);
+
+    if (!companyData) {
+        return (
+            <div>
+                LOADING...
+            </div>
+        )
+    }
 
     return (
         <div>
-            COMPANY DETAILS
-            Company name received: {handle}
+            <h2>{companyData.name}</h2>
+            <p>
+                {companyData.description}
+            </p>
         </div>
     )
 }
