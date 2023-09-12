c5c2cbda16cecc5594e70596213951307a9bda24
alexm <alex-m@outlook.com>
2023-09-11 18:14:01 -0700

    Render routes with some dummy prop functions

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
 import React from 'react';
 import {BrowserRouter} from "react-router-dom";
 
+import Routes from './routes/Routes';
 import './App.css';
 
 
 function App() {
     return (
         <div className="App">
-
+            <BrowserRouter>
+                <Routes
+                    login={() => {console.log("Running the login function")}}
+                    signup={() => {console.log("Running the signup function")}}
+                />
+            </BrowserRouter>
         </div>
     );
 }
