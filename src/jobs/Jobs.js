d58f44d40576066899c993e8d3f6ab0ee9db49be
alexm <alex-m@outlook.com>
2023-09-12 19:36:50 -0700

    Fetch jobs from API; implement search functionality; render job list on page

--- src/jobs/Jobs.js
+++ src/jobs/Jobs.js
@@ -1,11 +1,51 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
+
+import JoblyApi from "../api";
+import SearchBar from "../shared/SearchBar";
+import JobCard from "./JobCard";
 
 
 function Jobs() {
+    const [jobList, setJobList] = useState(null);
+
+    useEffect(() => {
+        const fetchJobsOnMount = async () => {
+            await searchFor();
+        }
+
+        fetchJobsOnMount();
+    }, []);
+
+    const searchFor = async (title) => {
+        try {
+            const results = await JoblyApi.getJobs(title);
+            setJobList(results);
+        } catch(err) {
+            console.log(err);
+        }
+    }
+
+    if (!jobList) return <div>LOADING...</div>
 
     return (
         <div>
-            JOB LIST
+            <SearchBar searchFor={searchFor}/>
+
+            {jobList.length ? (
+                <div>
+                    {jobList.map((job) => {
+                        return <JobCard
+                            key={job.id}
+                            title={job.title}
+                            salary={job.salary}
+                            equity={job.equity}
+                            companyName={job.companyName}
+                        />
+                    })}
+                </div>
+            ) : (
+                <p>No results found!</p>
+            )}
         </div>
     )
 }
