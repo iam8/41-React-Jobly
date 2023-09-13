30287c725d0e7d567dd868bab57ed1ffcac2b721
alexm <alex-m@outlook.com>
2023-09-12 19:14:25 -0700

    Set up user context provider and pass in some initial values (unfinished)

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {BrowserRouter} from "react-router-dom";
 
 import NavBar from './navbar/NavBar';
 import Routes from './routes/Routes';
+import UserContext from './auth/UserContext';
 import './App.css';
 
 
 function App() {
+    const [currentUser, setCurrentUser] = useState(null);
+
+    function hasAppliedToJob(id) {
+
+    }
+
+    function applyToJob(id) {
+
+    }
+
     return (
         <div className="App">
             <BrowserRouter>
-                <NavBar logout={() => {console.log("Running the logout function")}}/>
+                <UserContext.Provider
+                    value={{currentUser, setCurrentUser, hasAppliedToJob, applyToJob}}
+                >
+                    <NavBar logout={() => {console.log("Running the logout function")}}/>
 
-                <main>
                     <Routes
                         login={() => {console.log("Running the login function")}}
                         signup={() => {console.log("Running the signup function")}}
                     />
-                </main>
+
+                </UserContext.Provider>
             </BrowserRouter>
         </div>
     );
