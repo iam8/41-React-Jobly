9447d796ba5a1a20ad43a33375852d0f2077b89d
alexm <alex-m@outlook.com>
2023-09-13 16:02:38 -0700

    Create login form and add state and event handlers

--- src/auth/LoginForm.js
+++ src/auth/LoginForm.js
@@ -1,11 +1,55 @@
-import React from "react";
+import React, {useState} from "react";
 
 
-function LoginForm() {
+function LoginForm({login}) {
+    const INIT_FORM = {
+        username: "",
+        password: ""
+    };
+
+    const [formData, setFormData] = useState(INIT_FORM);
+
+    const handleChange = (evt) => {
+        const {name, value} = evt.target;
+        setFormData((formData) => ({
+            ...formData,
+            [name]: value
+        }));
+    }
+
+    const handleSubmit = async (evt) => {
+        evt.preventDefault();
+        await login(formData);
+        setFormData(INIT_FORM);
+    }
 
     return (
         <div>
-            LOGIN FORM
+            <form onSubmit={handleSubmit}>
+                <div>
+                    <label htmlFor="username">Username:</label>
+                    <input
+                        id="username"
+                        name="username"
+                        type="text"
+                        value={formData.username}
+                        onChange={handleChange}
+                    />
+                </div>
+
+                <div>
+                    <label htmlFor="password">Password:</label>
+                    <input
+                        id="password"
+                        name="password"
+                        type="password"
+                        value={formData.password}
+                        onChange={handleChange}
+                    />
+                </div>
+
+                <button>Submit</button>
+            </form>
         </div>
     )
 }
