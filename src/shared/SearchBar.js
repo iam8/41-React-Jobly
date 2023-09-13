e8ddecea116a20b1f4670d9532a138ec7b160488
alexm <alex-m@outlook.com>
2023-09-12 18:39:20 -0700

    Render search bar form and add search bar functionality

--- src/shared/SearchBar.js
+++ src/shared/SearchBar.js
@@ -1,11 +1,32 @@
-import React from "react";
+import React, {useState} from "react";
 
 
-function SearchBar() {
+function SearchBar({searchFor}) {
+    const [searchTerm, setSearchTerm] = useState("");
+
+    const handleSubmit = (evt) => {
+        evt.preventDefault();
+        searchFor(searchTerm.trim() || undefined);
+        setSearchTerm(searchTerm.trim());
+    }
+
+    const handleChange = (evt) => {
+        setSearchTerm(evt.target.value);
+    }
 
     return (
         <div>
-            SEARCH BAR
+            <form onSubmit={handleSubmit}>
+                <input
+                    name="searchTerm"
+                    placeholder="Enter a search term"
+                    value={searchTerm}
+                    onChange={handleChange}
+                />
+                <button type="submit">
+                    Search
+                </button>
+            </form>
         </div>
     )
 }
