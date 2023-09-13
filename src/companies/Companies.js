752ac5dbf8d68f44aea53670a91583b7b675cd24
alexm <alex-m@outlook.com>
2023-09-12 17:33:24 -0700

    Add logic to retrieve company list and render it on page

--- src/companies/Companies.js
+++ src/companies/Companies.js
@@ -1,11 +1,37 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
+
+import JoblyApi from "../api";
+import CompanyCard from "./CompanyCard";
 
 
 function Companies() {
+    const [companyList, setCompanyList] = useState(null);
+
+    useEffect(() => {
+        const fetchCompaniesOnMount = async () => {
+            try {
+                setCompanyList(await JoblyApi.getAllCompanies());
+            } catch(err) {
+                console.log(err);
+            }
+        };
+
+        fetchCompaniesOnMount();
+    }, []);
+
+    if (!companyList) return <div>LOADING...</div>
 
     return (
         <div>
-            COMPANY LIST
+            {companyList.map((company) => {
+                return <CompanyCard
+                    key={company.handle}
+                    name={company.name}
+                    desc={company.description}
+                    logoUrl={company.logo_url}
+                    handle={company.handle}
+                />
+            })}
         </div>
     )
 }
