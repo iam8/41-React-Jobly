698a6eb3edf0cb79f6ae95cb1726ff17ffc1a06a
alexm <alex-m@outlook.com>
2023-09-13 14:54:59 -0700

    Create component that renders a list of JobCard components, given a list of job data

---
+++ src/jobs/JobCardList.js
@@ -0,0 +1,25 @@
+import React from "react";
+
+import JobCard from "./JobCard";
+
+
+/** Render a list of JobCard components. */
+function JobCardList({jobList}) {
+
+    return (
+        <div>
+            {jobList.map((job) => {
+                return <JobCard
+                    key={job.id}
+                    title={job.title}
+                    salary={job.salary}
+                    equity={job.equity}
+                    companyName={job.companyName}
+                />
+            })}
+        </div>
+    )
+}
+
+
+export default JobCardList;
