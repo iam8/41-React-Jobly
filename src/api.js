2e07e0e8e573c5971fba9f0d30353cb5b7bdef09
alexm <alex-m@outlook.com>
2023-09-12 17:30:49 -0700

    Add method to get list of all companies

--- src/api.js
+++ src/api.js
@@ -36,8 +36,13 @@ class JoblyApi {
 
   // Individual API routes
 
-  /** Get details on a company by handle. */
+  /** Get list of all companies. */
+  static async getAllCompanies() {
+    let res = await this.request("companies/");
+    return res.companies;
+  }
 
+  /** Get details on a company by handle. */
   static async getCompany(handle) {
     let res = await this.request(`companies/${handle}`);
     return res.company;
