5ff508b2fbba0e68f414de045d51d32a4d86b024
alexm <alex-m@outlook.com>
2023-09-13 19:10:44 -0700

    Display a different message on homepage if user is logged in vs logged out

--- src/homepage/Home.js
+++ src/homepage/Home.js
@@ -1,11 +1,42 @@
-import React from "react";
+import React, {useContext} from "react";
+import { Link } from "react-router-dom";
+
+import UserContext from "../auth/UserContext";
 
 
 function Home() {
+    const {currentUser} = useContext(UserContext);
+
+    function loggedOutHome() {
+        return (
+            <>
+                <div>
+                    Sign in or log in
+                </div>
+            </>
+        )
+    }
+
+    function loggedInHome() {
+        return (
+            <>
+                <h2>Welcome, {currentUser.username}</h2>
+            </>
+        )
+    }
 
     return (
-        <div>
-            HOMEPAGE
+        <div className="Home">
+            <h1>Jobly</h1>
+            <h3>Apply for jobs from one spot, for free</h3>
+
+            {
+                currentUser ?
+                    loggedInHome()
+                :
+                    loggedOutHome()
+            }
+
         </div>
     )
 }
