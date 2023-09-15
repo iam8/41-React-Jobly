eff8fc2aea933fba11a375ac43320c343557baf9
alexm <alex-m@outlook.com>
2023-09-14 18:53:22 -0700

    Add docstring and comments to SearchBar component.

--- src/shared/SearchBar.js
+++ src/shared/SearchBar.js
@@ -1,15 +1,22 @@
 import React, {useState} from "react";
 
 
+/**
+ * Search bar. Used on /companies and /jobs pages to allow filtering.
+ *
+ * Calls the searchFor method (passed by parent), which performs the actual filtering.
+ */
 function SearchBar({searchFor}) {
     const [searchTerm, setSearchTerm] = useState("");
 
+    /** Call searchFor() to perform the filtering. */
     const handleSubmit = (evt) => {
         evt.preventDefault();
         searchFor(searchTerm.trim() || undefined);
         setSearchTerm(searchTerm.trim());
     }
 
+    /** Update search bar field. */
     const handleChange = (evt) => {
         setSearchTerm(evt.target.value);
     }
