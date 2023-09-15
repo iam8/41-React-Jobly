ca20b19935385ef87fab584da4b6be8d19628135
alexm <alex-m@outlook.com>
2023-09-14 18:48:12 -0700

    Add docstring to useLocalStorage hook

--- src/hooks/useLocalStorage.js
+++ src/hooks/useLocalStorage.js
@@ -1,6 +1,19 @@
 import { useState, useEffect } from "react";
 
 
+/**
+ * Custom hook for syncing state data (item) with local storage.
+ *
+ * Params:
+ *  - key (string): the key corresponding to the item in local storage. Used to retrieve the item.
+ *  - defaultValue: initial item value to store, if the given key doesn't exist in local storage.
+ *
+ * When the stored item changes:
+ *  - If item is null, it is removed from local storage
+ *  - Otherwise, stores the item with the given key
+ *
+ * Returns [item, setItem] for reading and updating the item in state.
+ */
 function useLocalStorage(key, defaultValue=null) {
     const initItem = localStorage.getItem(key) || defaultValue;
     const [item, setItem] = useState(initItem);
