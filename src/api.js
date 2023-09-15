55625cd7c247396978063f8345a4e348e76d65c4
alexm <alex-m@outlook.com>
2023-09-14 18:27:22 -0700

    Adjust indentation; edit existing comments and add docstrings for API methods.

--- src/api.js
+++ src/api.js
@@ -2,95 +2,91 @@ import axios from "axios";
 
 const BASE_URL = process.env.REACT_APP_BASE_URL || "http://localhost:3001";
 
-/** API Class.
+
+/** Jobly API Class.
  *
  * Static class tying together methods used to get/send to to the API.
- * There shouldn't be any frontend-specific stuff here, and there shouldn't
- * be any API-aware stuff elsewhere in the frontend.
- *
  */
-
 class JoblyApi {
-  // the token for interactive with the API will be stored here.
-  static token;
+    static token;
 
-  static async request(endpoint, data = {}, method = "get") {
-    console.debug("API Call:", endpoint, data, method);
+    /** Send request to endpoint via Axios with (optional) data and method. */
+    static async request(endpoint, data = {}, method = "get") {
+        console.debug("API Call:", endpoint, data, method);
 
-    //there are multiple ways to pass an authorization token, this is how you pass it in the header.
-    //this has been provided to show you another way to pass the token. you are only expected to read this code for this project.
-    const url = `${BASE_URL}/${endpoint}`;
-    const headers = { Authorization: `Bearer ${JoblyApi.token}` };
-    const params = (method === "get")
-        ? data
-        : {};
+        // Pass authorization token via request header.
+        const url = `${BASE_URL}/${endpoint}`;
+        const headers = { Authorization: `Bearer ${JoblyApi.token}` };
+        const params = (method === "get")
+            ? data
+            : {};
 
-    try {
-      return (await axios({ url, method, data, params, headers })).data;
-    } catch (err) {
-      console.error("API Error:", err.response);
-      let message = err.response.data.error.message;
-      throw Array.isArray(message) ? message : [message];
+        try {
+            return (await axios({ url, method, data, params, headers })).data;
+        } catch (err) {
+            console.error("API Error:", err.response);
+            let message = err.response.data.error.message;
+            throw Array.isArray(message) ? message : [message];
+        }
     }
-  }
 
-  // Individual API routes
+    // Individual API routes ----------------------------------------------------------------------
 
-  /** Register a new user. */
-  static async signup({username, password, firstName, lastName, email}) {
-    let res = await this.request("auth/register",
-                                 {username, password, firstName, lastName, email},
-                                 "post");
-    return res.token;
-  }
+    /** Register a new user and return their generated auth token. */
+    static async signup({username, password, firstName, lastName, email}) {
+        let res = await this.request("auth/register",
+                                    {username, password, firstName, lastName, email},
+                                    "post");
+        return res.token;
+    }
 
-  /** Log in a user. */
-  static async login({username, password}) {
-    let res = await this.request("auth/token", {username, password}, "post");
-    return res.token;
-  }
+    /** Log in a user and return their auth token. */
+    static async login({username, password}) {
+        let res = await this.request("auth/token", {username, password}, "post");
+        return res.token;
+    }
 
-  /** Get a user by username. */
-  static async getCurrentUser(username) {
-    let res = await this.request(`users/${username}`);
-    return res.user;
-  }
+    /** Get a user by username. */
+    static async getCurrentUser(username) {
+        let res = await this.request(`users/${username}`);
+        return res.user;
+    }
 
-  /** Get list of all companies. */
-  static async getCompanies(name) {
-    let res = await this.request("companies/", {name});
-    return res.companies;
-  }
+    /** Search for and return all companies with names like the given name. */
+    static async getCompanies(name) {
+        let res = await this.request("companies/", {name});
+        return res.companies;
+    }
 
-  /** Get details on a company by handle. */
-  static async getCompany(handle) {
-    let res = await this.request(`companies/${handle}`);
-    return res.company;
-  }
+    /** Get details on a company by handle. */
+    static async getCompany(handle) {
+        let res = await this.request(`companies/${handle}`);
+        return res.company;
+    }
 
-  /** Get list of all jobs. */
-  static async getJobs(title) {
-    let res = await this.request("jobs/", {title});
-    return res.jobs;
-  }
+    /** Search for and return all jobs with titles like the given title. */
+    static async getJobs(title) {
+        let res = await this.request("jobs/", {title});
+        return res.jobs;
+    }
 
-  /** Apply to a job. */
+    /** Apply to a job with the given jobId using the given username and return the job ID on
+     * success. */
     static async applyToJob(username, jobId) {
         let res = await this.request(`users/${username}/jobs/${jobId}`, {}, "post");
         return res.applied;
     }
 
-    /** Edit user profile. */
+    /** Save new user profile info. */
     static async saveUserProfile({username, firstName, lastName, email}) {
         let res = await this.request(`users/${username}`,
                                      {firstName, lastName, email},
                                      "patch");
         return res.user;
     }
-
 }
 
-// for now, put token ("testuser" / "password" on class)
+// For now, put token ("testuser" / "password" on class)
 JoblyApi.token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZ" +
     "SI6InRlc3R1c2VyIiwiaXNBZG1pbiI6ZmFsc2UsImlhdCI6MTU5ODE1OTI1OX0." +
     "FtrMwBQwe6Ue-glIFgz_Nf8XxRT2YecFCiSpYL0fCXc";
