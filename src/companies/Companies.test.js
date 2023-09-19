abc6d84818676baabafe64b821133772da85c975
alexm <alex-m@outlook.com>
2023-09-18 19:16:06 -0700

    Add smoke test

--- src/companies/Companies.test.js
+++ src/companies/Companies.test.js
@@ -3,6 +3,11 @@ import { render } from "@testing-library/react";
 import Companies from "./Companies";
 
 
+test("Renders without crashing", () => {
+    render (<Companies />);
+})
+
+
 test("Matches snapshot", () => {
     const {asFragment} = render(<Companies />);
     expect(asFragment()).toMatchSnapshot();
